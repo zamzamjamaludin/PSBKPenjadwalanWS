c00f201a127ccf1b699ced5168388bf9f77eceee
endang <endang.hidayat0912@gmail.com>
2016-04-21 19:21:41 +0700

    add jersey json full implement modul V

---
+++ src/main/java/com/psbk/ws/Student.java
@@ -0,0 +1,39 @@
+package com.psbk.ws;
+
+public class Student {
+
+	private String id;
+	private String name;
+	private String address;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return "Student : "+id+" - "+name;
+	}
+
+}
