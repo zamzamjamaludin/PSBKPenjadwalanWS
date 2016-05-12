1520d5150df9f085ac78bde1b9c36885cf97b681
zamzamjamaludin <zamzam.jamaludin@mail.unpas.ac.id>
2016-05-12 22:22:22 +0700

    Select Jadwal

---
+++ src/main/java/com/psbk/ws/Jadwal.java
@@ -0,0 +1,49 @@
+package com.psbk.ws;
+
+public class Jadwal {
+
+	private int id;
+	private String hari;
+	private String jam;
+	private String id_matpel;
+	private int id_ruangan;
+	private String id_dosen;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getHari() {
+		return hari;
+	}
+	public void setHari(String hari) {
+		this.hari = hari;
+	}
+	public String getJam() {
+		return jam;
+	}
+	public void setJam(String jam) {
+		this.jam = jam;
+	}
+	public String getId_matpel() {
+		return id_matpel;
+	}
+	public void setId_matpel(String id_matpel) {
+		this.id_matpel = id_matpel;
+	}
+	public int getId_ruangan() {
+		return id_ruangan;
+	}
+	public void setId_ruangan(int id_ruangan) {
+		this.id_ruangan = id_ruangan;
+	}
+	public String getId_dosen() {
+		return id_dosen;
+	}
+	public void setId_dosen(String id_dosen) {
+		this.id_dosen = id_dosen;
+	}
+	
+	
+}
---
+++ src/main/java/com/psbk/ws/JadwalService.java
@@ -0,0 +1,94 @@
+package com.psbk.ws;
+
+import com.psbk.ws.common.MasterConnection;
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import org.codehaus.jettison.json.JSONObject;
+import com.psbk.ws.common.MyMap;
+
+@Path("/jadwal")
+public class JadwalService extends MasterConnection{
+	@GET
+	@Path("{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Map getJadwalByID(@PathParam("id") String id){
+		Map<String, Object> result = new HashMap<String, Object>();
+		result.put("statusId", "1");
+		result.put("message", "INQUIRY BERHASIL");
+		System.out.println("id : "+id);
+		try {
+			createConnection();
+			MyMap jadwal = (MyMap)jt.queryObject("select d.nama, m.nama, j.hari, j.jam, r.nama "
+					+ "from jadwal j, dosen d, matpel m, ruangan r "
+					+ "where  j.id_matpel=m.id and j.id_dosen=d.id and j.id_ruangan=r.id and  j.id = ?", new Object[] {id}, new MyMap());
+			closeConnection();
+			if (jadwal != null){
+				result.put("result", jadwal);
+			}
+		} catch (Exception e) {
+			result.put("Message", "GAGAL KARENA : " +e.getMessage());
+		}
+		
+		return result;
+	}
+	
+	@POST
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/insert")
+	public Object createMatpel(@Context HttpServletRequest hsr){
+		StringBuffer sb = new StringBuffer();
+		String line = null;
+		JSONObject request = null;
+		MyMap respon = new MyMap();
+		MyMap data = new MyMap();
+		DataInputStream in;
+		
+		try {
+			createConnection();
+			in = new DataInputStream(new BufferedInputStream(hsr.getInputStream()));
+			
+			while ((line = in.readLine()) != null)
+				sb.append(line);
+			JSONObject json = new JSONObject(sb.toString());
+			request = (JSONObject) json.get("request");
+			
+			if(request == null){
+				respon.put("message", "Data yang dikirim tidak ditemukan");
+				respon.put("rCode", "99");
+				respon.put("statusId", "0");
+				
+				return respon;
+			}
+			
+			data.put("id", request.getInt("id"));
+			data.put("hari", request.getString("hari"));
+			data.put("jam", request.getString("jam"));
+			data.put("id_matpel", request.getString("id_matpel"));
+			data.put("id_ruangan", request.getInt("id_ruangan"));
+			data.put("id_dosen", request.getString("id_dosen"));
+			
+			jt.insert("jadwal", data);// insert ke tabel
+			respon.put("message", "DATA BERHASIL DISIMPAN");
+			respon.put("rCode", "00");
+			respon.put("statusId", "1");
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			respon.put("message", e.getMessage());
+			respon.put("rCode", "99");
+			respon.put("statusId", "0");
+		}
+		return respon;
+	}
+}
