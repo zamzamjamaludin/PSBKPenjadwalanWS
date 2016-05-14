640d28dcac0cb4f24fb07eb4e0542189a24b0d05
zamzamjamaludin <zamzam.jamaludin@mail.unpas.ac.id>
2016-05-14 07:10:10 +0700

    update database

--- src/main/resources/penjadwalan.sql
+++ src/main/resources/penjadwalan.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: May 13, 2016 at 03:04 PM
+-- Generation Time: May 14, 2016 at 02:09 AM
 -- Server version: 10.1.10-MariaDB
 -- PHP Version: 7.0.4
 
@@ -37,8 +37,8 @@ CREATE TABLE `dosen` (
 --
 
 INSERT INTO `dosen` (`id`, `nama`, `alamat`) VALUES
-('1097566738', 'A', 'A'),
-('1097566734', 'B', 'B');
+('1097566738', 'Budi', 'Soekarno Hatta'),
+('1097566734', 'Ani', 'Setiabudhi');
 
 -- --------------------------------------------------------
 
@@ -52,17 +52,41 @@ CREATE TABLE `jadwal` (
   `jam` varchar(13) NOT NULL,
   `id_matpel` varchar(5) NOT NULL,
   `id_dosen` varchar(12) NOT NULL,
-  `id_ruangan` int(3) NOT NULL
+  `id_ruangan` int(3) NOT NULL,
+  `nrp` longtext NOT NULL,
+  `kelas` char(1) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `jadwal`
 --
 
-INSERT INTO `jadwal` (`id`, `hari`, `jam`, `id_matpel`, `id_dosen`, `id_ruangan`) VALUES
-(1, 'Senin', '07.00 - 09.30', 'IF201', '1097566738', 1),
-(2, 'Selasa', '07.00 - 09.30', 'IF201', '1097566734', 2),
-(3, 'Rabu', '13.00 - 15.20', 'IF101', '1097566738', 1);
+INSERT INTO `jadwal` (`id`, `hari`, `jam`, `id_matpel`, `id_dosen`, `id_ruangan`, `nrp`, `kelas`) VALUES
+(1, 'Senin', '07.00 - 09.30', 'IF201', '1097566738', 1, '', 'A'),
+(2, 'Selasa', '07.00 - 09.30', 'IF201', '1097566734', 2, '', 'C'),
+(3, 'Sabtu', '08.00 - 10.30', 'IF301', '1097566734', 2, '', 'A');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jadwal_mahasiswa`
+--
+
+CREATE TABLE `jadwal_mahasiswa` (
+  `id_jadwal` int(5) DEFAULT NULL,
+  `nrp` varchar(9) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `jadwal_mahasiswa`
+--
+
+INSERT INTO `jadwal_mahasiswa` (`id_jadwal`, `nrp`) VALUES
+(1, '133040053'),
+(2, '133040053'),
+(3, '133040046'),
+(2, '133040046'),
+(3, '133040053');
 
 -- --------------------------------------------------------
 
@@ -82,8 +106,7 @@ CREATE TABLE `mahasiswa` (
 
 INSERT INTO `mahasiswa` (`nrp`, `nama`, `alamat`) VALUES
 ('133040053', 'Zamzam Jamaludin', 'Jl. Rancamanyar'),
-('133040046', 'Rayi Sundara', 'Antapani'),
-('133040053', 'Zamzam', 'Jl. Rancamanyar');
+('133040046', 'Rayi Sundara', 'Antapani');
 
 -- --------------------------------------------------------
 
